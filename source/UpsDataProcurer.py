da5531a196ce7c8e7dcca30eba6c479f07b0eaaa
Will Walker <williamwalker753@gmail.com>
2024-01-09 02:27:16 -0600

    init commit

---
+++ source/UpsDataProcurer.py
@@ -0,0 +1,16 @@
+import requests
+
+class UpsDataProcurer:
+    def __init__(self):
+        pass
+
+    def procure(self):
+        # url = 'http://192.168.0.181/sub/apcups'
+        # headers = {
+        #     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        #     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+        #     # Add other headers here if needed
+        # }
+
+        # # Send the GET request
+        # response = requests.get(url, headers=headers)
